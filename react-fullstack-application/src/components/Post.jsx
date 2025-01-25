14c13d64d2f519e2feccb5b9f25c3ebf70cc601d
chandansingh-tech <chandan.kumr.singh@gmail.com>
2025-01-25 19:53:38 +0530

    Added initial code for component

---
+++ react-fullstack-application/src/components/Post.jsx
@@ -0,0 +1,14 @@
+const names = ['Kumar', 'Chandan'];
+
+function Post() {
+
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
+    return (
+      <div>
+        <p>{chosenName}</p>
+      </div>
+    );
+  }
+  
+  export default Post;
